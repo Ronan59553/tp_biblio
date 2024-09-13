1cb7d8a29e459234d2461c685554642b4ee98a9c
Bertrand Moty <Bertrand Mot>
2024-09-13 15:07:57 +0200

    ajout de la base de données

---
+++ GITHUB.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : ven. 13 sep. 2024 à 13:03
+-- Version du serveur : 10.11.4-MariaDB-1~deb12u1
+-- Version de PHP : 8.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `github`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Livres`
+--
+
+CREATE TABLE `Livres` (
+  `id_livres` int(11) NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `auteur` varchar(50) NOT NULL,
+  `date_publi` date NOT NULL,
+  `genre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `id_livre` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `date_reservation` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `date_inscription` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `mot_de_passe`, `date_inscription`) VALUES
+(2, 'efzfezf', 'zfefezfz', 'moty@rer', '$2y$10$LWiPleglhKtkO2VPQ/e5deTf1Etw6cloLtkOrfrn3A8/O7CnAVQeu', '2024-09-11 09:06:51'),
+(5, 'Moty', 'Bertrand', 'motybebert@gmail.com', '$2y$10$9P4N23yN9A73q.DpBuMN2OxmG/VPWiCR.QCiG6OSnL6iKv9dbcjwy', '2024-09-11 09:19:11'),
+(6, 'Schaubroeck', 'Ronan', 'roschau59@gmail.com', '$2y$10$IxWdaXLBSGOVZzPWtDnM.Oz.MwxeiSmYMi7dzVqEyzdo.iyOrE5Oq', '2024-09-12 07:09:13');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Livres`
+--
+ALTER TABLE `Livres`
+  ADD PRIMARY KEY (`id_livres`);
+
+--
+-- Index pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_livre` (`id_livre`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Livres`
+--
+ALTER TABLE `Livres`
+  MODIFY `id_livres` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`id_livre`) REFERENCES `Livres` (`id_livres`),
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
