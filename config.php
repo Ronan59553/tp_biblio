dcd6a1e844ee28742142946b1aa4d1733e80ffdd
Bertrand Moty <Bertrand Mot>
2024-09-26 11:00:34 +0200

    enregistrement des modifactions locales avant la fusion

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'votre_utilisateur', 'votre_mot_de_passe', 'GITHUB');
-
-if ($mysqli->connect_error) {
-    die('Erreur de connexion (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
-?>
